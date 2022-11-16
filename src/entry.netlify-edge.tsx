98252614f790acb4b69cc7d6c488a1576566aad8
j1ngzoue <workdrive0306@gmail.com>
2022-11-16 23:19:41 +0900

    chore: debug on netlify

--- src/entry.netlify-edge.tsx
+++ src/entry.netlify-edge.tsx
@@ -1,6 +1,11 @@
+import type { Context } from "@netlify/edge-functions";
 import { qwikCity } from "@builder.io/qwik-city/middleware/netlify-edge";
 import render from "./entry.ssr";
 
-const qwikCityHandler = qwikCity(render);
+const qwikCityHandler = (request: Request, context: Context) => {
+  console.log(request);
+  console.log(context);
+  return qwikCity(render);
+};
 
 export default qwikCityHandler;
